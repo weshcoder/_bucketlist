c7b5c0e9458bb21729422fbaf586aa57498e14fd
Your Name <you@example.com>
2019-08-29 12:03:01 +0300

    pylint

--- UI/run.py
+++ UI/run.py
@@ -7,24 +7,29 @@ def home():
 @app.route("/createbucket")
 def bucket():
     return render_template("createbucket.html")
+
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
+
 @app.route("/signin")
 def signin():
     return render_template("signin.html")
+
 @app.route("/forgot")
 def forgot():
     return render_template("forgot.html")
+
 @app.route("/resetpassword")
 def reset():
     return render_template("resetpassword.html")
+
 @app.route("/editbucket")
 def editbucket():
     return render_template("editbucket.html")
+
 @app.route("/items")
 def items():
     return render_template("items.html")
-
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
