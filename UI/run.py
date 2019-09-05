a975a6c07293aac7fedc533a079716e8109abd93
Your Name <you@example.com>
2019-09-05 11:52:46 +0300

    unit testing

--- UI/run.py
+++ UI/run.py
@@ -1,35 +1,7 @@
-from flask import Flask
-from flask import render_template
-app = Flask(__name__, static_url_path='/static')
-@app.route("/home")
-def home():
-    return render_template("index.html")
-@app.route("/createbucket")
-def bucket():
-    return render_template("createbucket.html")
+import os
+from .app import APP
 
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
 
-@app.route("/signin")
-def signin():
-    return render_template("signin.html")
-
-@app.route("/forgot")
-def forgot():
-    return render_template("forgot.html")
-
-@app.route("/resetpassword")
-def reset():
-    return render_template("resetpassword.html")
-
-@app.route("/editbucket")
-def editbucket():
-    return render_template("editbucket.html")
-
-@app.route("/items")
-def items():
-    return render_template("items.html")
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get('PORT', 5000))
+    APP.run(port=port)
---
+++ UI/tests/test_routes.py
@@ -0,0 +1,14 @@
+import unittest
+from . import app
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.client = app.test_client
+
+    def test_home(self):
+        response = self.client().get('/home')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
